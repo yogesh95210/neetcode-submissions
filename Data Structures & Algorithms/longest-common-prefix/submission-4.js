82d720f07dd5a013a618df7cd63557ee2c0660fe
Sharma_Yogesh <91339421+yogesh95210@users.noreply.github.com>
2026-08-21 21:30:50 +0530

    Add: longest-common-prefix - submission-4

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-4.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string}
+     */
+    longestCommonPrefix(strs) {
+      let prefix= strs[0]
+      for (let i=1; i<strs.length;i++){
+
+        while( !strs[i].startsWith(prefix)){
+           prefix= prefix.slice(0,-1)
+
+           if(prefix===""){
+            return ""
+           }
+        }
+      }
+      return prefix
+    }
+}
