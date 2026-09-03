9d93843fb11ebafd80c4123270855a6d7a0eb7d4
Sharma_Yogesh <91339421+yogesh95210@users.noreply.github.com>
2026-09-03 20:18:21 +0530

    Add: remove-duplicates-from-sorted-array - submission-7

---
+++ Data Structures & Algorithms/remove-duplicates-from-sorted-array/submission-7.js
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    removeDuplicates(nums) {
+        let l=1
+        for(let r=0;r<nums.length;r++){
+            if(nums[r] !==nums[l-1]){
+                nums[l]= nums[r]
+                l++
+            }
+        }
+        return l
+    }
+}
