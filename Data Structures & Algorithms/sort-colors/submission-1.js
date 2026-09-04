43bbad2e0a7bbe41fd9489b514bc31430ae659da
Sharma_Yogesh <91339421+yogesh95210@users.noreply.github.com>
2026-09-04 17:47:36 +0530

    Add: sort-colors - submission-1

---
+++ Data Structures & Algorithms/sort-colors/submission-1.js
@@ -0,0 +1,9 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {void} Do not return anything, modify nums in-place instead.
+     */
+    sortColors(nums) {
+        return nums.sort((a,b)=>a-b)
+    }
+}
